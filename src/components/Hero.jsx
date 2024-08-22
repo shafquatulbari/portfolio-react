c18542965b99792e66301e7292a6dcad0e4eeaa4
shafquatulbari <77532312+shafquatulbari@users.noreply.github.com>
2024-08-22 20:10:55 +0600

    hero section & navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { AvatarCanvas, ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#41b5f7]" />
+          <div className="w-1 sm:h-80 h-40 bg-[#1b4d6a]" />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#41b5f7]">Shafquat Ul Bari</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            a passionate Computer Science graduate from the University of
+            British Columbia <br className="sm:block hidden" />
+            with a strong background in software engineering, machine learning,
+            and game development.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 bg-[#41b4f72c] flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-[#41b4f7] mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/canvas/Avatar.jsx
@@ -0,0 +1,74 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Avatar = ({ isMobile }) => {
+  // Update the path to your GLB file here
+  const avatar = useGLTF("src/assets/avatar/avatar.glb");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={avatar.scene}
+        scale={isMobile ? 0.7 : 0.75} // Adjust scale if needed
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} // Adjust position if needed
+        rotation={[-0.01, -0.2, -0.1]} // Adjust rotation if needed
+      />
+    </mesh>
+  );
+};
+
+const AvatarCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Avatar isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default AvatarCanvas;
