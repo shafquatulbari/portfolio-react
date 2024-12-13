48eeedc4a019b6c8d5394cb565a5a899e9637e56
shafquatulbari <gkid175@gmail.com>
2024-12-13 14:50:57 +0600

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,39 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <div className="bg-tech-pattern bg-cover bg-no-repeat bg-center">
-          <About />
+        <div className="bg-tech-pattern bg-cover bg-no-repeat bg-center relative">
+          <div className="absolute inset-0 bg-black bg-opacity-30 z-10"></div>
+          <div className="relative z-20">
+            <About />
+          </div>
         </div>
-        <div className="bg-work-pattern bg-cover bg-no-repeat bg-center">
-          <Experience />
+        <div className="bg-work-pattern bg-cover bg-no-repeat bg-center relative">
+          <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+          <div className="relative z-20">
+            <Experience />
+          </div>
         </div>
-        <div className="bg-road-pattern bg-cover bg-no-repeat bg-center">
-          <Tech />
+
+        <div className="bg-road-pattern bg-cover bg-no-repeat bg-center relative">
+          <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+          <div className="relative z-20">
+            <Tech />
+          </div>
         </div>
-        <div className="bg-rain-pattern bg-cover bg-no-repeat bg-center">
-          <Works />
+
+        <div className="bg-rain-pattern bg-cover bg-no-repeat bg-center relative">
+          <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+          <div className="relative z-20">
+            <Works />
+          </div>
         </div>
-        <div className="bg-universe-pattern bg-cover bg-no-repeat bg-center">
-          <Contact />
-          <Footer />
+
+        <div className="bg-universe-pattern bg-cover bg-no-repeat bg-center relative">
+          <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+          <div className="relative z-20">
+            <Contact />
+            <Footer />
+          </div>
         </div>
       </div>
     </BrowserRouter>
