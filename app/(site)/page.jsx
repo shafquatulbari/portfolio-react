4af6bed9b184902bbbbf22b677594fc850462a21
shafquatulbari <gkid175@gmail.com>
2025-08-10 16:18:22 -0400

    reattempt deploy

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,5 +1,8 @@
 // Route entry is a Server Component. Client logic lives in ClientHome.
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import ClientHome from "./ClientHome";
 
 export default function Page() {
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,1 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export { default } from "./(site)/page";
